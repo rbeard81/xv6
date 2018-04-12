4b283049a3feebcf5f51131e2194afc6fe9b07f6
rbeard81 <33492835+rbeard81@users.noreply.github.com>
2018-04-12 15:09:02 -0400

    Add files via upload

---
+++ user/workload.d
@@ -0,0 +1,2 @@
+user/workload.d user/workload.o: user/workload.c include/types.h \
+ include/stat.h user/user.h include/fcntl.h
---
+++ user/wrkld.d
@@ -0,0 +1,2 @@
+user/wrkld.d user/wrkld.o: user/wrkld.c include/types.h include/stat.h \
+ user/user.h include/fcntl.h
