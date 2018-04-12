ecd1ac3f477ea2eb0d9eceb31e9d55a2866fce2e
rbeard81 <33492835+rbeard81@users.noreply.github.com>
2018-04-12 15:55:18 -0400

    update 4b

--- kernel/defs.h
+++ kernel/defs.h
@@ -109,8 +109,8 @@ void            userinit(void);
 int             wait(void);
 void            wakeup(void*);
 void            yield(void);
-int 			pstatus(void);
-int				settickets(int);
+int		          clone(void(*fcn)(void*), void*, void*);
+int		          join(void**);
 
 // swtch.S
 void            swtch(struct context**, struct context*);
